439233392240a4a36f4237c69ada5e01d785b786
ZachWagner1 <zwag14@yahoo.com>
2020-07-18 22:10:00 -0500

    generate markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,52 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const {
+    title, 
+    description, 
+    installation, 
+    usage, 
+    contributing, 
+    techInstructions,
+    license,
+    github,
+    email
+  } = data;
+    return `# ${data.title}
+![${license} license badge](https://img.sheilds.io/badge/license-${license.replace(/ /g, '%20')}-green)
 
+## Description
+
+${description}
+
+## Table of Contents
+  *[Installation](#installation)
+  *[Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  * 
+  * ## Installation
+  ${installation}
+
+  ## Usage
+  ${usage}
+
+  ## License 
+  This project is covered under the ${license} license.
+
+  ## Contributing
+  ${contributing}
+
+  ## Testing
+  ${testInstructions}
+
+  ## Questions
+  Visit me at GitHub  
+  [${github}](https://github.com/${github})
+
+  Comments or questions? Email me at:
+  [${email}](mailto:${email})
 `;
 }
 
